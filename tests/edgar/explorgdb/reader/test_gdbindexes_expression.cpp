e4c22262bbef7f2f6c9a294a3dc13785ba6bf301
EdgarLQS <52236485+EdgarLQS@users.noreply.github.com>
2026-07-17 17:19:07 +0800

    test(reader): cover functional index expression classification

---
+++ tests/edgar/explorgdb/reader/test_gdbindexes_expression.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+
+#include "gdb_indexes.h"
+
+using namespace explorgdb;
+
+TEST(GdbIndexesExpressionTest, DirectFieldRemainsDirect) {
+    EXPECT_EQ(GdbIndexesParser::field_name_from_expression("Name"),
+              "Name");
+    EXPECT_TRUE(GdbIndexesParser::is_direct_field_expression("Name"));
+}
+
+TEST(GdbIndexesExpressionTest, LowerExpressionMapsFieldButIsNotDirect) {
+    EXPECT_EQ(GdbIndexesParser::field_name_from_expression("LOWER(Name)"),
+              "Name");
+    EXPECT_EQ(GdbIndexesParser::field_name_from_expression("lower(Name)"),
+              "Name");
+    EXPECT_FALSE(
+        GdbIndexesParser::is_direct_field_expression("LOWER(Name)"));
+}
+
+TEST(GdbIndexesExpressionTest, UnknownExpressionIsNotMisclassifiedAsLower) {
+    EXPECT_EQ(GdbIndexesParser::field_name_from_expression("UPPER(Name)"),
+              "UPPER(Name)");
+    EXPECT_TRUE(
+        GdbIndexesParser::is_direct_field_expression("UPPER(Name)"));
+    EXPECT_EQ(GdbIndexesParser::field_name_from_expression("LOWER(Name"),
+              "LOWER(Name");
+}
